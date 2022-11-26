46099becd456c5f6a473355398df7711fe84b50d
Arvin Vitug <arvinvitug@gmail.com>
2022-11-25 19:24:50 -0500

    Update descriptions for click and command

--- src/cli.py
+++ src/cli.py
@@ -4,26 +4,32 @@ import discord_bot
 
 @click.command()
 def discord():
+    """Run discord bot"""
     discord_bot.run()
 
 @click.command()
 def fa():
+    """Get top 50 free agents"""
     print(espn_responses.get_free_agents())
 
 @click.command()
 def injuries():
+    """Get top 50 injured playeres"""
     print(espn_responses.get_injured_players())
 
 @click.command()
 def teamstats():
+    """Get record for all fantasy league teams"""
     print(espn_responses.get_win_loss())
     
 @click.command()
 def scoreboard():
+    """Get score for this week's fantasy league matchups"""
     print(espn_responses.get_score_board())
 
 @click.group
 def commands():
+    """CLI for interfacing with an ESPN NBA fantasy league"""
     pass
 
 commands.add_command(discord)
